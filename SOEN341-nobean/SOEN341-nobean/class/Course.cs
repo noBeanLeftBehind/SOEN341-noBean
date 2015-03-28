1f2d1dd2384b8956e80f5a2beeca2f364d20d9cd
thomasCM23 <2utom23@gmail.com>
2015-03-28 15:15:41 -0700

    0.0.4

--- SOEN341-nobean/SOEN341-nobean/class/Course.cs
+++ SOEN341-nobean/SOEN341-nobean/class/Course.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -92,6 +94,5 @@ namespace SOEN341_nobean.Class
             }
             return true;
         }
-
     }
 }
