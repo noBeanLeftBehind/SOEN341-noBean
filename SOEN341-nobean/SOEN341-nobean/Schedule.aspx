7f513a4ea46103fbf42b2806ce6789be237a6f0f
JonathanH <jonathanhamel4@gmail.com>
2015-03-29 10:43:54 -0400

    added some functions to schedule.cs and daypilot in schedule.aspx

--- SOEN341-nobean/SOEN341-nobean/Schedule.aspx
+++ SOEN341-nobean/SOEN341-nobean/Schedule.aspx
@@ -1,8 +1,34 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterPage.Master" AutoEventWireup="true" CodeBehind="Schedule.aspx.cs" Inherits="SOEN341_nobean.WebForm2" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterPage.Master" AutoEventWireup="true" CodeBehind="Schedule.aspx.cs" Inherits="SOEN341_nobean.Schedule" %>
+<%@ Register assembly="DayPilot" namespace="DayPilot.Web.Ui" tagprefix="DayPilot" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
     <link rel="stylesheet" href="master.css" type="text/css" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Stylesheets" runat="server">
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>Current schedule</p>
+    <!--<asp:Table runat="server" ID="winter2016Schedule">
+        <asp:TableHeaderRow>
+            <asp:TableHeaderCell>
+            </asp:TableHeaderCell>
+            <asp:TableHeaderCell>
+            Monday
+            </asp:TableHeaderCell>
+            <asp:TableHeaderCell>
+            Tuesday
+            </asp:TableHeaderCell>
+            <asp:TableHeaderCell>
+            Wednesday
+            </asp:TableHeaderCell>
+            <asp:TableHeaderCell>
+            Thursday
+            </asp:TableHeaderCell>
+            <asp:TableHeaderCell>
+            Friday
+            </asp:TableHeaderCell>
+        </asp:TableHeaderRow>
+    </asp:Table>-->
+    <DayPilot:DayPilotCalendar ID="DayPilotCalendar1" runat="server" />
+    <br />
+
 </asp:Content>
--- SOEN341-nobean/SOEN341-nobean/Schedule.aspx.cs
+++ SOEN341-nobean/SOEN341-nobean/Schedule.aspx.cs
@@ -4,14 +4,71 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SOEN341_nobean.Class;
 
 namespace SOEN341_nobean
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class Schedule : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                //5 hardcoded courses
+                //If I do not make SOEN341_nobean.Class.Schedule();
+                //then it will be automatically SOEN341_nobean.Schedule();... why no .Class?
+                //I will not enter all courses as I need to enter all possible sections...I will just make it fit in the schedule, to see look and feel
+                //var winter2016 = new SOEN341_nobean.Class.Schedule(); 
+                //Course engr391 = new Course("ENGR", "391", "Numerical MethodCs", 3);
+                //Course soen344 = new Course("SOEN", "344", "Architecture Design", 3);
+                //Course soen345 = new Course("SOEN", "345", "Testing and verification", 3);
+                //Course soen357 = new Course("SOEN", "357", "User Interface Design", 3);
+                //Course soen390= new Course("SOEN", "390", "Software Team Project", 3);
 
+                //creating the list of course to be added to the List<List<Course>> semesters
+                //List<Course> winter2016Courses = new List<Course>(new Course[] { engr391, soen344, soen345, soen357, soen390 });
+                //winter2016.addASemester(winter2016Courses);
+                //makeAspTable(winter2016);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        /*
+         * If DayPilot does not work, one may generate the schedule with asp:Table and generating TableRows programmatically
+         * 
+         * */
+        private void makeAspTable(SOEN341_nobean.Class.Schedule schedule)
+        {
+            DateTime dt = DateTime.Parse("7/01/2016 07:00:00 AM");
+            //This comparison should stop when the last class has finished.
+            while(dt.Hour != 22)
+            {
+                TableRow row = new TableRow();
+                TableCell time = new TableCell();
+                TableCell monday = new TableCell();
+                TableCell tuesday = new TableCell();
+                TableCell wednesday = new TableCell();
+                TableCell Thursday = new TableCell();
+                TableCell Friday = new TableCell();
+                time.Text = String.Format("{0:HH:mm}", dt);
+                
+                // I will enter manually courses in the schedule to see how it fits in it....
+                //There should be if statement whether the course starts at this time, then row span.
+                    //if(engr391.startTime()==dt)
+
+                //adding all the cells
+                row.Cells.Add(time);
+                row.Cells.Add(monday);
+                row.Cells.Add(time);
+                row.Cells.Add(time);
+                row.Cells.Add(time);
+                //adding the row to the asp table
+                winter2016Schedule.Rows.Add(row);
+                //adding 15 minutes for the next row
+                dt = dt.AddMinutes(15);
+            }
         }
     }
 }
