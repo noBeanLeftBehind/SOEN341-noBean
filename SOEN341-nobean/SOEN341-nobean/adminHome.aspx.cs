d9de8b7a573e2d40573802cdf02f97ba321411a7
JonathanH <jonathanhamel4@gmail.com>
2015-04-07 15:43:17 -0400

    Admin have their own users.

--- SOEN341-nobean/SOEN341-nobean/adminHome.aspx.cs
+++ SOEN341-nobean/SOEN341-nobean/adminHome.aspx.cs
@@ -12,14 +12,17 @@ namespace SOEN341_nobean
 {
     public partial class adminHome : System.Web.UI.Page
     {
-        private User student;
+
+        private static User student;
+        
         private DBHandler userHandler = new DBHandler();
         protected void Page_Load(object sender, EventArgs e)
         {
+                Global.Admin = Global.MainUser;
                 if (Global.myConnection == null || Global.myConnection.State == System.Data.ConnectionState.Closed || Global.MainUser == null)
                     Response.Redirect("login.aspx");
 
-                if (Global.MainUser.getisAdmin() == false)
+                if (Global.Admin.getisAdmin() == false)
                     Response.Redirect("home.aspx");
 
                 SubmitIDButton.Click += new EventHandler(this.SubmitIDButton_Click);
@@ -40,9 +43,17 @@ namespace SOEN341_nobean
 
                     try
                     {
-                        student = userHandler.getUserByID(studentID);
-                        if (student.getfirstName() != null && student.getlastName() != null)
+                        User tempStudent = userHandler.getUserByID(studentID);
+                        if (tempStudent.getisAdmin())
                         {
+                            error_IDStudent.Style["color"] = "red";
+                            error_IDStudent.Text = String.Format("You have not the rights to log in as an administrator", studentIDTextBox.Text);
+                            connectStudent.Visible = false;
+                            connectStudent.Visible = false;
+                        }
+                        else if (tempStudent.getfirstName() != null && tempStudent.getlastName() != null)
+                        {
+                            student = tempStudent;
                             hiddenStudentID.Value = studentID;
                             error_IDStudent.Text = "Student found: ";
                             LabelStudentFound.Text = String.Format("<p>\r\n {0}, {1} </p>", student.getlastName(), student.getfirstName());
@@ -54,9 +65,9 @@ namespace SOEN341_nobean
                         {
                             error_IDStudent.Style["color"] = "red";
                             error_IDStudent.Text = String.Format("Student with ID {0} was not found", studentIDTextBox.Text);
+                            connectStudent.Visible = false;
+                            connectStudent.Visible = false;
                         }
-                        
-                      
                     }
                     catch (Exception ex)
                     {
@@ -69,15 +80,18 @@ namespace SOEN341_nobean
             {
                 error_IDStudent.Text = "ERROR: Enter a 8 digit Student ID";
                 LabelStudentFound.Text = "";
+                connectStudent.Visible = false;
+                connectStudent.Visible = false;
             }
 
         }
 
         protected void connectStudent_Click1(object sender, EventArgs e)
         {
-            Global.MainUser = userHandler.getUserByID(hiddenStudentID.Value);
+            Global.MainUser = student;
             Response.Redirect("home.aspx");
         }
 
-    }
+    
+public  SOEN341_nobean.User _student { get; set; }}
 }
