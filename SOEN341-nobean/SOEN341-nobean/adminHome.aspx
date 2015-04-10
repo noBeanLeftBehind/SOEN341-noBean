54e63e5840377184fd2fd10d58be1711ec35fe18
JonathanH <jonathanhamel4@gmail.com>
2015-04-10 10:29:16 -0400

    Using student viewstate

--- SOEN341-nobean/SOEN341-nobean/adminHome.aspx
+++ SOEN341-nobean/SOEN341-nobean/adminHome.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/masterPage.master" CodeBehind="adminHome.aspx.cs" Inherits="SOEN341_nobean.adminHome" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/masterPage.master" EnableViewState="true" EnableViewStateMAC="true" ViewStateEncryptionMode="Always" CodeBehind="adminHome.aspx.cs" Inherits="SOEN341_nobean.adminHome" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head"  runat="server">
     <link rel="stylesheet" href="master.css" type="text/css" />
--- SOEN341-nobean/SOEN341-nobean/adminHome.aspx.cs
+++ SOEN341-nobean/SOEN341-nobean/adminHome.aspx.cs
@@ -12,21 +12,18 @@ namespace SOEN341_nobean
 {
     public partial class adminHome : System.Web.UI.Page
     {
-
-        private static User student;
-        
         private DBHandler userHandler = new DBHandler();
         protected void Page_Load(object sender, EventArgs e)
         {
-                if (Global.myConnection == null || Global.myConnection.State == System.Data.ConnectionState.Closed || Global.MainUser == null)
-                    Response.Redirect("login.aspx");
+            if (Global.myConnection == null || Global.myConnection.State == System.Data.ConnectionState.Closed || Global.MainUser == null)
+                Response.Redirect("login.aspx");
 
-                if (Global.MainUser.getisAdmin() == false)
-                    Response.Redirect("home.aspx");
-                Global.Admin = Global.MainUser;
+            if (Global.MainUser.getisAdmin() == false)
+                Response.Redirect("home.aspx");
+            Global.Admin = Global.MainUser;
 
-                SubmitIDButton.Click += new EventHandler(this.SubmitIDButton_Click);
-                connectStudent.Click += new EventHandler(this.connectStudent_Click1);
+            SubmitIDButton.Click += new EventHandler(this.SubmitIDButton_Click);
+            connectStudent.Click += new EventHandler(this.connectStudent_Click1);
         }
 
         private void SubmitIDButton_Click(object sender, EventArgs e)
@@ -41,40 +38,43 @@ namespace SOEN341_nobean
                 error_IDStudent.Style["color"] = "black";
                 error_IDStudent.Text = String.Format("Searching for {0}...", studentIDTextBox.Text);
 
-                    try
+                try
+                {
+                    User tempStudent = userHandler.getUserByID(studentID);
+                   if (tempStudent != null) { 
+                    if (tempStudent.getisAdmin())
                     {
-                        User tempStudent = userHandler.getUserByID(studentID);
-                        if (tempStudent.getisAdmin())
-                        {
-                            error_IDStudent.Style["color"] = "red";
-                            error_IDStudent.Text = String.Format("You have not the rights to log in as an administrator", studentIDTextBox.Text);
-                            connectStudent.Visible = false;
-                            connectStudent.Visible = false;
-                        }
-                        else if (tempStudent.getfirstName() != null && tempStudent.getlastName() != null)
-                        {
-                            student = tempStudent;
-                            hiddenStudentID.Value = studentID;
-                            error_IDStudent.Text = "Student found: ";
-                            LabelStudentFound.Text = String.Format("<p>\r\n {0}, {1} </p>", student.getlastName(), student.getfirstName());
-                            studentIDTextBox.Text = "";
-                            connectStudent.Visible = true;
-                            connectStudent.Visible = true;
-                        }
-                        else
-                        {
-                            error_IDStudent.Style["color"] = "red";
-                            error_IDStudent.Text = String.Format("Student with ID {0} was not found", studentIDTextBox.Text);
-                            connectStudent.Visible = false;
-                            connectStudent.Visible = false;
-                        }
+                        error_IDStudent.Style["color"] = "red";
+                        error_IDStudent.Text = String.Format("You have not the rights to log in as an administrator", studentIDTextBox.Text);
+                        connectStudent.Visible = false;
+                        LabelStudentFound.Text = "";
                     }
-                    catch (Exception ex)
+                    else if (tempStudent.getfirstName() != null && tempStudent.getlastName() != null)
                     {
-                        Response.Write("Error:" + ex.ToString());
+                        ViewState["StudentViewState"] = tempStudent;
+                        hiddenStudentID.Value = studentID;
+                        error_IDStudent.Style["color"] = "black";
+                        error_IDStudent.Text = "Student found: ";
+                        LabelStudentFound.Text = String.Format("<p>\r\n {0}, {1} </p>", tempStudent.getlastName(), tempStudent.getfirstName());
+                        studentIDTextBox.Text = "";
+                        connectStudent.Visible = true;
+                    }
+                   }
+                   else
+                    {
+                        error_IDStudent.Style["color"] = "red";
+                        error_IDStudent.Text = String.Format("Student with ID {0} was not found", studentIDTextBox.Text);
+                        connectStudent.Visible = false;
+                        LabelStudentFound.Text = "";
+                    }
+                       
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Error:" + ex.ToString());
 
-                    }
-                            
+                }
+
             }
             else
             {
@@ -88,10 +88,8 @@ namespace SOEN341_nobean
 
         protected void connectStudent_Click1(object sender, EventArgs e)
         {
-            Global.MainUser = student;
+            Global.MainUser = (User)ViewState["StudentViewState"];
             Response.Redirect("home.aspx");
         }
-
-    
-public  SOEN341_nobean.User _student { get; set; }}
+    }
 }
