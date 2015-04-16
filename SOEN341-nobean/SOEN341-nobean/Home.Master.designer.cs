70fb083105f5d07cd000dc19d77d58606b65301a
JonathanH <jonathanhamel4@gmail.com>
2015-04-16 01:19:27 -0400

    menu for admin

--- SOEN341-nobean/SOEN341-nobean/Home.Master.designer.cs
+++ SOEN341-nobean/SOEN341-nobean/Home.Master.designer.cs
@@ -49,6 +49,15 @@ namespace SOEN341_nobean {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl accountNavID;
         
         /// <summary>
+        /// recordNavID control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl recordNavID;
+        
+        /// <summary>
         /// log control.
         /// </summary>
         /// <remarks>
